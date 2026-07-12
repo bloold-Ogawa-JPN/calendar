dfc0df3dc82cfc107ec5482496004115606638ed
bloold-Ogawa-JPN <blo.old.ogawa@gmail.com>
2026-07-12 13:01:36 +0900

    Update script.js

--- script.js
+++ script.js
@@ -12,9 +12,15 @@ document.addEventListener("DOMContentLoaded", () => {
 
   // 1. ダークモード切り替え
   themeToggle.addEventListener("click", () => {
-    document.body.classList.toggle("dark");
-    document.body.classList.toggle("light");
+    if (document.body.classList.contains("dark")) {
+      document.body.classList.remove("dark");
+      document.body.classList.add("light");
+    } else {
+      document.body.classList.remove("light");
+      document.body.classList.add("dark");
+    }
   });
+
 
   // 2. カレンダー描画ロジック
   function renderCalendar(date) {
